a3a38e04ad7f297ef19136e6525c51c1153f35b0
David Lozano <dawgun@hotmail.com>
2022-08-31 11:00:24 +0200

    Added style with style components

---
+++ src/pages/RegisterPage/RegisterPageStyled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const RegisterPageStyled = styled.section`
+  background-color: #292929;
+  height: 100vh;
+
+  .register-page {
+    &__title {
+      color: white;
+      margin: 0;
+      padding-left: 1rem;
+      padding-top: 1rem;
+    }
+  }
+`;
+
+export default RegisterPageStyled;
