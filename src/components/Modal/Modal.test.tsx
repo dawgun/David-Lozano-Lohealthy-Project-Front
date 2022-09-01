79d4208ba171c15180e7832a3ab821e1e5f63ad7
David Lozano <dawgun@hotmail.com>
2022-09-01 11:36:47 +0200

    Added test for Modal component

---
+++ src/components/Modal/Modal.test.tsx
@@ -0,0 +1,81 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import Modal from "./Modal";
+import userEvent from "@testing-library/user-event";
+
+let mockSelectorReturn = {
+  isModalShowing: false,
+  message: "This test is awesome",
+  type: true,
+};
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+  useSelector: () => mockSelectorReturn,
+}));
+
+describe("Given a Modal component", () => {
+  describe("When instantiated", () => {
+    const expectedText = "This test is awesome";
+    test("Then it should show 'This test is awesome'", async () => {
+      render(
+        <Provider store={store}>
+          <Modal />
+        </Provider>
+      );
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    test("Then it should show a green background color", async () => {
+      render(
+        <Provider store={store}>
+          <Modal />
+        </Provider>
+      );
+
+      const modal = screen.getByText(expectedText);
+      const modalStyle = getComputedStyle(modal);
+
+      expect(modalStyle.backgroundColor).toBe("rgb(74, 172, 14)");
+    });
+
+    test("Then it should show a red background color", async () => {
+      mockSelectorReturn = {
+        isModalShowing: false,
+        message: "This test is awesome",
+        type: false,
+      };
+
+      render(
+        <Provider store={store}>
+          <Modal />
+        </Provider>
+      );
+
+      const modal = screen.getByText(expectedText);
+      const modalStyle = getComputedStyle(modal);
+
+      expect(modalStyle.backgroundColor).toBe("rgb(212, 53, 33)");
+    });
+
+    test("Then it should show the message it have received", async () => {
+      render(
+        <Provider store={store}>
+          <Modal />
+        </Provider>
+      );
+
+      const button = screen.getByRole("button", { name: "X" });
+      await userEvent.click(button);
+
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  });
+});
