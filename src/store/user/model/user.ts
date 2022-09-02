e401764e081a082fd97f73b756779030a2f622f7
David Lozano <dawgun@hotmail.com>
2022-09-02 17:58:35 +0200

    Created model for User and UserLoginState

---
+++ src/store/user/model/user.ts
@@ -0,0 +1,12 @@
+export interface UserLoginState {
+  isLogged: boolean;
+  user: {
+    userName: string;
+    image: string;
+  };
+}
+
+export interface User {
+  userName: string;
+  image: string;
+}
