cb9c70c4db9ddf01c30292068ae4f5e960af7527
David Lozano <dawgun@hotmail.com>
2022-09-04 14:00:59 +0200

    Added variable state in App for give header menu button and tested

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,20 +1,33 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event/";
 import Header from "./Header";
 
 describe("Given the Header component", () => {
   describe("When it's instantiated", () => {
     test("Then it should show Lohealthy Games in heading", () => {
-      render(<Header />);
+      render(<Header actionOnClick={() => {}} />);
       const title = screen.getByRole("heading", { name: "Lohealthy Games" });
 
       expect(title).toBeInTheDocument();
     });
 
     test("Then it should show a button", () => {
-      render(<Header />);
+      render(<Header actionOnClick={() => {}} />);
       const button = screen.getByRole("button");
 
       expect(button).toBeInTheDocument();
+    });
+
+    describe("And users click on button", () => {
+      test("Then it should be called the function", async () => {
+        const mockFunction = jest.fn();
+
+        render(<Header actionOnClick={mockFunction} />);
+        const button = screen.getByRole("button");
+        await userEvent.click(button);
+
+        expect(mockFunction).toHaveBeenCalled();
+      });
     });
   });
 });
