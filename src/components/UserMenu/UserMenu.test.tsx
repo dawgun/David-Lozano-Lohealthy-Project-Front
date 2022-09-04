f0c610bdd052611fbd5b24856e89f1b5b4f54453
David Lozano <dawgun@hotmail.com>
2022-09-04 14:01:36 +0200

    Created test for UserMenu component

---
+++ src/components/UserMenu/UserMenu.test.tsx
@@ -0,0 +1,94 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../store/store";
+import UserMenu from "./UserMenu";
+
+const mockSelectorReturn = {
+  isLogged: false,
+  user: {
+    userName: "Nachus",
+    image: "",
+  },
+};
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: () => mockSelectorReturn,
+}));
+
+describe("Given the UserMenu component", () => {
+  describe("When user is not logged", () => {
+    test("Then should show two nav links", async () => {
+      const expectedLenght = 2;
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <UserMenu actionOnClick={() => {}} />
+          </BrowserRouter>
+        </Provider>
+      );
+      const navlinks = await screen.findAllByRole("link");
+
+      expect(navlinks).toHaveLength(expectedLenght);
+    });
+
+    describe("And user click on 'Registrar' or 'Login'", () => {
+      test("Then should functions has been called", async () => {
+        const mockFunction = jest.fn();
+        const calledTimes = 2;
+
+        render(
+          <Provider store={store}>
+            <BrowserRouter>
+              <UserMenu actionOnClick={mockFunction} />
+            </BrowserRouter>
+          </Provider>
+        );
+        const navlinks = await screen.findAllByRole("link");
+        await userEvent.click(navlinks[0]);
+        await userEvent.click(navlinks[1]);
+
+        expect(mockFunction).toHaveBeenCalledTimes(calledTimes);
+      });
+    });
+  });
+
+  describe("When user is logged", () => {
+    test("Then should show an user name 'Nachus' and image with alternativeText 'Profile pic representing user'", async () => {
+      const userName = "Nachus";
+      const alternativeText = "Profile pic representing user";
+      mockSelectorReturn.isLogged = true;
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <UserMenu actionOnClick={() => {}} />
+          </BrowserRouter>
+        </Provider>
+      );
+      const text = screen.getByText(userName);
+      const image = screen.getByRole("img", { name: alternativeText });
+
+      expect(text).toBeInTheDocument();
+      expect(image).toBeInTheDocument();
+    });
+
+    test("Then should show 'Logout'", async () => {
+      const logoutText = "Logout";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <UserMenu actionOnClick={() => {}} />
+          </BrowserRouter>
+        </Provider>
+      );
+      const text = screen.getByText(logoutText);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
