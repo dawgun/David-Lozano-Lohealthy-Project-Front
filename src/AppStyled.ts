04a38a9dbaa50acffd4fddc26bdef483b28cc11d
David Lozano <dawgun@hotmail.com>
2022-09-12 01:20:42 +0200

    Added section with Header, Nav and UserMenu for sticky style

--- src/AppStyled.ts
+++ src/AppStyled.ts
@@ -4,6 +4,12 @@ const AppStyled = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+
+  .menu-container {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+  }
 `;
 
 export default AppStyled;
