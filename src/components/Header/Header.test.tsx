79cdc854f5ee9ce8faffd07435338dc6bfe9e0bb
David Lozano <dawgun@hotmail.com>
2022-09-12 00:45:21 +0200

    Modified Header component with menu changes and tested

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,18 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event/";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import { toggleMenuActionCreator } from "../../store/UI/UISlice";
 import Header from "./Header";
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
 
 describe("Given the Header component", () => {
   describe("When it's instantiated", () => {
     test("Then it should show Lohealthy Games in heading", () => {
-      render(<Header actionOnClick={() => {}} />);
+      render(
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      );
       const title = screen.getByRole("heading", { name: "Lohealthy Games" });
 
       expect(title).toBeInTheDocument();
     });
 
     test("Then it should show a button", () => {
-      render(<Header actionOnClick={() => {}} />);
+      render(
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      );
       const button = screen.getByRole("button");
 
       expect(button).toBeInTheDocument();
@@ -20,13 +38,15 @@ describe("Given the Header component", () => {
 
     describe("And users click on button", () => {
       test("Then it should be called the function", async () => {
-        const mockFunction = jest.fn();
-
-        render(<Header actionOnClick={mockFunction} />);
+        render(
+          <Provider store={store}>
+            <Header />
+          </Provider>
+        );
         const button = screen.getByRole("button");
         await userEvent.click(button);
 
-        expect(mockFunction).toHaveBeenCalled();
+        expect(mockDispatch).toHaveBeenCalledWith(toggleMenuActionCreator());
       });
     });
   });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
+import { useAppDispatch } from "../../store/hooks";
+import { toggleMenuActionCreator } from "../../store/UI/UISlice";
 import HeaderStyled from "./HeaderStyled";
 
-interface HeaderProps {
-  actionOnClick: () => void;
-}
+const Header = (): JSX.Element => {
+  const dispatch = useAppDispatch();
 
-const Header = ({ actionOnClick }: HeaderProps): JSX.Element => {
+  const handlerToggleMenu = () => {
+    dispatch(toggleMenuActionCreator());
+  };
+
   return (
     <HeaderStyled className="header">
       <h1 className="header-title">
         <span className="header-title__first-part">Lohealthy</span>
         <span className="header-title__second-part">Games</span>
       </h1>
-      <button className="header-button" onClick={actionOnClick}>
+      <button className="header-button" onClick={handlerToggleMenu}>
         <img
           className="header-button__icon"
           alt="Menu icon"
