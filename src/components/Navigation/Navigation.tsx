e9199a9430979409212b128c5e56c12551b508fe
David Lozano <dawgun@hotmail.com>
2022-09-12 01:02:58 +0200

    Added closeMenu to navlinks and removed Juegos nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,32 @@
 import { NavLink } from "react-router-dom";
+import { useAppDispatch } from "../../store/hooks";
+import { closeMenuActionCreator } from "../../store/UI/UISlice";
 import NavigationStyled from "./NavigationStyled";
 
 const Navigation = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const closeMenuHandler = () => {
+    dispatch(closeMenuActionCreator());
+  };
+
   return (
     <NavigationStyled className="navigation">
-      <NavLink to={"/home"} className="nav-link">
-        <img src="/images/home_icon.svg" alt="Home icon"></img>
+      <NavLink onClick={closeMenuHandler} to={"/home"} className="nav-link">
+        <img
+          height={18}
+          width={18}
+          src="/images/home_icon.svg"
+          alt="Home icon"
+        ></img>
       </NavLink>
-      <div className="navigation__right-side">
-        <NavLink to={"/juegos"} className="nav-link">
-          Juegos
-        </NavLink>
-        <NavLink to={"/mis-juegos"} className="nav-link">
-          Mis Juegos
-        </NavLink>
-      </div>
+      <NavLink
+        onClick={closeMenuHandler}
+        to={"/mis-juegos"}
+        className="nav-link"
+      >
+        Mis Juegos
+      </NavLink>
     </NavigationStyled>
   );
 };
