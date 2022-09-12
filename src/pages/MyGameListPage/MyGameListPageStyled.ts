fb7230e93628d91987dcaea27c0dc75ed806eb47
David Lozano <dawgun@hotmail.com>
2022-09-12 16:50:05 +0200

    Added styled to MyGameListPage with styled components

---
+++ src/pages/MyGameListPage/MyGameListPageStyled.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const MyGameListPageStyled = styled.section`
+  padding: 1rem;
+
+  .my-games {
+    &__container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    &__title {
+      color: #858383;
+    }
+
+    &__button {
+      background-color: #d43521;
+      font-size: 1rem;
+      color: #ffffff;
+      border: 0px;
+      width: 20rem;
+      text-transform: uppercase;
+      padding: 1rem;
+      margin: 1rem 0;
+      font-family: "Roboto";
+      font-weight: bold;
+    }
+  }
+`;
+
+export default MyGameListPageStyled;
