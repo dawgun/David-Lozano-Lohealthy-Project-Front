32582790fad353cc955cbe707af05c203d123d11
David Lozano <dawgun@hotmail.com>
2022-09-12 16:49:40 +0200

    Created MyGameListPage page

---
+++ src/pages/MyGameListPage/MyGameListPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useGames from "../../hooks/useGames/useGames";
+import MyGameListPageStyled from "./MyGameListPageStyled";
+import GameCardList from "../../components/GameCardList/GameCardList";
+
+const MyGameListPage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const { getGamesByUser } = useGames();
+
+  const navigateHandler = () => {
+    navigate("/mis-juegos/create");
+  };
+
+  useEffect(() => {
+    getGamesByUser();
+  }, [getGamesByUser]);
+
+  return (
+    <MyGameListPageStyled className="my-games__page">
+      <h2 className="my-games__title">Mis Juegos</h2>
+      <div className="my-games__container">
+        <button className="my-games__button" onClick={navigateHandler}>
+          Crear Juego
+        </button>
+        <GameCardList />
+      </div>
+    </MyGameListPageStyled>
+  );
+};
+
+export default MyGameListPage;
