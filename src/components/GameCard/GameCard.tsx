7f867e8c2b66a885dcd90a72f7016ad00afcd6d3
David Lozano <dawgun@hotmail.com>
2022-09-13 15:09:36 +0200

    Modified button for change in different path

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -2,8 +2,7 @@ import useGames from "../../hooks/useGames/useGames";
 import { Game } from "../../store/games/model/game";
 import { useAppSelector } from "../../store/hooks";
 import GameCardStyled from "./GameCardStyled";
-import { TiDelete } from "react-icons/ti";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface GameCardProps {
   game: Game;
@@ -13,9 +12,10 @@ const GameCard = ({ game }: GameCardProps): JSX.Element => {
   const { deleteGame } = useGames();
   const { user } = useAppSelector((state) => state.user);
   const navigate = useNavigate();
-  const maxLettersSynopsis = 100;
+  const maxLettersSynopsis = 80;
   const isSamePerson = game.owner === user.id;
   const urlAPI = process.env.REACT_APP_API_URL;
+  const { pathname } = useLocation();
 
   const infoHandler = () => {
     navigate(`/details/${game.id}`);
@@ -24,8 +24,8 @@ const GameCard = ({ game }: GameCardProps): JSX.Element => {
   return (
     <GameCardStyled className="game">
       <img
-        width={320}
-        height={180}
+        width={90}
+        height={160}
         src={`${urlAPI}${game.image}`}
         alt={`${game.title} game`}
         onError={({ currentTarget }) => {
@@ -36,19 +36,24 @@ const GameCard = ({ game }: GameCardProps): JSX.Element => {
       ></img>
       <div className="game__info">
         <h3 className="game__title">{game.title}</h3>
-        <div className="game__details">
-          <p className="game__synopsis">{`${game.synopsis.slice(
-            0,
-            maxLettersSynopsis
-          )}...`}</p>
+        <p className="game__synopsis">{`${game.synopsis.slice(
+          0,
+          maxLettersSynopsis
+        )}...`}</p>
+        {pathname === "/home" && (
           <button className="game__button" onClick={infoHandler}>
             Info
           </button>
-        </div>
+        )}
+        {pathname === "/mis-juegos" && (
+          <button className="game__button" onClick={infoHandler}>
+            Edit
+          </button>
+        )}
       </div>
       {isSamePerson && (
         <button className="game__delete" onClick={() => deleteGame(game.id)}>
-          <TiDelete data-testid="delete-icon" />
+          ✗
         </button>
       )}
     </GameCardStyled>
