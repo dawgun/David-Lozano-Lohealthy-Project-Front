64d50d6518fa397aac9974c5985fa58d2e676374
David <dawgun@hotmail.com>
2023-03-01 14:22:55 +0100

    Create main theme with style components

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    primaryColor: "#292929",
+    secondaryColor: "#d43521",
+  },
+};
+
+export default mainTheme;
