7edbe9d8f621efba263e70646f9f2058e5dbe76c
David <dawgun@hotmail.com>
2023-03-01 12:14:21 +0100

    Create global style with style components replacing index css

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { store } from "./store/store";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import "@fontsource/roboto";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -15,13 +15,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <GlobalStyle />
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
