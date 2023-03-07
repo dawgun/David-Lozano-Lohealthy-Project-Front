e843294d7484e311ca3fe9dafd3a9d8c14801ef4
David <dawgun@hotmail.com>
2023-03-07 13:55:22 +0100

    Refactorice App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,183 +1,66 @@
 import mockDispatch from "../../testUtils/mocks/mockDispatch/mockDispatch";
 import { screen } from "@testing-library/react";
-import App from "./App";
 import { loginUserActionCreator } from "../../store/user/userSlice";
-import customRender from "../../testUtils/wrappers/customRender/customRender";
 import {
   initialUiState,
-  initialUserState,
   mockStore,
 } from "../../testUtils/mocks/mockStore/mockStore";
+import customRender from "../../testUtils/wrappers/customRender/customRender";
+import App from "./App";
 
 const mockUser = { userName: "Pedro", image: "", token: "token", id: "1" };
 
 jest.mock("../../utils/auth/auth", () => () => mockUser);
 
-describe("Given App component", () => {
+describe("When it's instantiated", () => {
   const store = mockStore({
     uiPreloadState: {
       ...initialUiState,
       isModalShowing: true,
       isLoadingShowing: true,
-      isMenuShowing: true,
     },
-    userPreloadState: { ...initialUserState, isLogged: true },
   });
 
-  describe("When it's instantiated", () => {
-    test("Then should show 'Cargando...' from Loading component", () => {
-      const loadingText = "Cargando...";
+  test("Then should show 'Cargando...' from Loading component", () => {
+    const loadingText = "Cargando...";
+
+    customRender(<App />, { store });
+
+    const text = screen.getByText(loadingText);
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test("Then should show a button with 'X' from Modal component", () => {
+    const textButton = "✕";
+
+    customRender(<App />, { store });
+
+    const button = screen.getByRole("button", { name: textButton });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Then should show 'Home' in a heading", () => {
+    const homeTitle = "Home";
+
+    customRender(<App />);
+
+    const headingHome = screen.getByRole("heading", { name: homeTitle });
+
+    expect(headingHome).toBeInTheDocument();
+  });
+
+  describe("And navigator have a token", () => {
+    test("Then dispatch has to been called with loginUser action", () => {
+      const loginUserAction = loginUserActionCreator(mockUser);
+
+      const mockToken = "mockToken";
+      window.localStorage.setItem("token", mockToken);
 
       customRender(<App />, { store });
 
-      const text = screen.getByText(loadingText);
-
-      expect(text).toBeInTheDocument();
-    });
-
-    test("Then should show a button with 'X' from Modal component", () => {
-      const textButton = "✕";
-
-      customRender(<App />, { store });
-
-      const button = screen.getByRole("button", { name: textButton });
-
-      expect(button).toBeInTheDocument();
-    });
-
-    test("Then should show 'Lohealthygames' in a heading from Header component", () => {
-      const titleText = "Lohealthy Games";
-
-      customRender(<App />, { store });
-
-      const title = screen.getByRole("heading", { name: titleText });
-
-      expect(title).toBeInTheDocument();
-    });
-
-    test("Then should show 'Mis Juegos' in a link from Navigation component", () => {
-      const navLinkText = "Mis Juegos";
-
-      customRender(<App />, { store });
-
-      const navLink = screen.getByRole("link", { name: navLinkText });
-
-      expect(navLink).toBeInTheDocument();
-    });
-
-    test("Then should show 'Home' in a heading from HomePage", () => {
-      const titleHome = "Home";
-
-      customRender(<App />, { store });
-
-      const title = screen.getByRole("heading", { name: titleHome });
-
-      expect(title).toBeInTheDocument();
-    });
-
-    test("Then should show a property text Footer component", () => {
-      const propertyText = "Lohealthy Games es propiedad de David Lozano.";
-
-      customRender(<App />, { store });
-
-      const text = screen.getByText(propertyText);
-
-      expect(text).toBeInTheDocument();
-    });
-
-    describe("And navigator have a token", () => {
-      test("Then dispatch has to been called with loginUser action", () => {
-        const loginUserAction = loginUserActionCreator(mockUser);
-
-        const mockToken = "mockToken";
-        window.localStorage.setItem("token", mockToken);
-
-        customRender(<App />, { store });
-
-        expect(mockDispatch).toHaveBeenCalledWith(loginUserAction);
-      });
-    });
-
-    describe("And path on navigator is '/error'", () => {
-      test("Then should show 'Mazmorra no encontrada' in a heading from NotFoundPage", () => {
-        const webPath = "/error";
-        const titleHeading = "Mazmorra no encontrada";
-
-        customRender(<App />, { initialEntries: [webPath] });
-
-        const title = screen.getByRole("heading", { name: titleHeading });
-
-        expect(title).toBeInTheDocument();
-      });
-    });
-
-    describe("And path on navigator is '/mis-juegos'", () => {
-      test("Then should show 'Mis Juegos' in a heading from MyGameListPage", () => {
-        const webPath = "/mis-juegos";
-        const titleText = "Mis Juegos";
-
-        customRender(<App />, { initialEntries: [webPath], store });
-
-        const title = screen.getByRole("heading", { name: titleText });
-
-        expect(title).toBeInTheDocument();
-      });
-    });
-
-    describe("And path on navigator is '/mis-juegos/create'", () => {
-      test("Then should show 'Crear Juego' in a heading from CreateGamePage", () => {
-        const webPath = "/mis-juegos/create";
-        const titleText = "Crear Juego";
-
-        customRender(<App />, { initialEntries: [webPath], store });
-
-        const title = screen.getByRole("heading", { name: titleText });
-
-        expect(title).toBeInTheDocument();
-      });
-    });
-
-    describe("And path on navigator is '/details/juego'", () => {
-      test("Then should show 'Detalles del juego' in a heading MyGameListPage", () => {
-        const webPath = "/details/juego";
-        const titleText = "Detalles del juego";
-
-        customRender(<App />, { initialEntries: [webPath] });
-
-        const title = screen.getByRole("heading", { name: titleText });
-
-        expect(title).toBeInTheDocument();
-      });
-    });
-
-    describe("And user is not logged and path on navigator is '/login'", () => {
-      test("Then should show 'Detalles del juego' in a heading MyGameListPage", () => {
-        const webPath = "/login";
-        const titleText = "Login";
-
-        customRender(<App />, {
-          initialEntries: [webPath],
-        });
-
-        const title = screen.getByRole("heading", { name: titleText });
-
-        expect(title).toBeInTheDocument();
-      });
-    });
-
-    describe("And user is not logged and path on navigator is '/register'", () => {
-      test("Then should show 'Detalles del juego' in a heading MyGameListPage", () => {
-        const webPath = "/register";
-        const titleText = "Registro";
-
-        customRender(<App />, {
-          initialEntries: [webPath],
-        });
-
-        const title = screen.getByRole("heading", { name: titleText });
-
-        expect(title).toBeInTheDocument();
-      });
+      expect(mockDispatch).toHaveBeenCalledWith(loginUserAction);
     });
   });
 });
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import AppStyled from "./AppStyled";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import Loading from "../Loading/Loading";
-import Modal from "../Modal/Modal";
-import Navigation from "../Navigation/Navigation";
-import ReverseRouteProtector from "../ReverseRouteProtector/ReverseRouteProtector";
-import RouteProtector from "../RouteProtector/RouteProtector";
-import UserMenu from "../UserMenu/UserMenu";
-import CreateGamePage from "../../pages/CreateGamePage/CreateGamePage";
-import GameDetailsPage from "../../pages/GameDetailsPage/GameDetailsPage";
-import HomePage from "../../pages/HomePage/HomePage";
-import LoginPage from "../../pages/LoginPage/LoginPage";
-import MyGameListPage from "../../pages/MyGameListPage/MyGameListPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { loginUserActionCreator } from "../../store/user/userSlice";
 import fetchToken from "../../utils/auth/auth";
-import pathRoutes from "../../utils/pathRoutes/pathRoutes";
+import Layout from "../Layout/Layout";
+import Loading from "../Loading/Loading";
+import Modal from "../Modal/Modal";
 import { useEffect } from "react";
 
-function App() {
-  const { isLoadingShowing, isModalShowing, isMenuShowing } = useAppSelector(
+function App(): JSX.Element {
+  const { isLoadingShowing, isModalShowing } = useAppSelector(
     (state) => state.ui
   );
   const token = localStorage.getItem("token");
   const dispatch = useAppDispatch();
-  const { home, details, all, createGame, login, myGames, register, root } =
-    pathRoutes;
 
   useEffect(() => {
     if (token) {
@@ -36,56 +19,12 @@ function App() {
       dispatch(loginUserActionCreator(user));
     }
   });
-
   return (
-    <AppStyled className="main-container">
+    <>
       {isLoadingShowing && <Loading />}
       {isModalShowing && <Modal />}
-      <section className="menu-container">
-        <Header />
-        <Navigation />
-        {isMenuShowing && <UserMenu />}
-      </section>
-      <Routes>
-        <Route path={root} element={<Navigate to={home} />} />
-        <Route path={home} element={<HomePage />} />
-        <Route path={details} element={<GameDetailsPage />} />
-        <Route
-          path={myGames}
-          element={
-            <RouteProtector>
-              <MyGameListPage />
-            </RouteProtector>
-          }
-        />
-        <Route
-          path={`${myGames}${createGame}`}
-          element={
-            <RouteProtector>
-              <CreateGamePage />
-            </RouteProtector>
-          }
-        />
-        <Route
-          path={register}
-          element={
-            <ReverseRouteProtector>
-              <RegisterPage />
-            </ReverseRouteProtector>
-          }
-        />
-        <Route
-          path={login}
-          element={
-            <ReverseRouteProtector>
-              <LoginPage />
-            </ReverseRouteProtector>
-          }
-        />
-        <Route path={all} element={<NotFoundPage />} />
-      </Routes>
-      <Footer />
-    </AppStyled>
+      <Layout />
+    </>
   );
 }
 
